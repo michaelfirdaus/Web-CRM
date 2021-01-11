37b988bfed0a17fa513e4bd5221b85a5639d9891
Michael <michaelfirdausmf@gmail.com>
2021-01-11 13:21:03 +0700

    Disabled delete feature on data kunci

--- resources/views/program/index.blade.php
+++ resources/views/program/index.blade.php
@@ -25,9 +25,6 @@
                     <th class="text-center">
                         Edit
                     </th>
-                    <th class="text-center">
-                        Hapus
-                    </th>
                 </thead>
         
                 <tbody>
@@ -48,38 +45,6 @@
                                         <span class="fas fa-pencil-alt"></span>
                                     </a>
                                 </td>
-                                <td class="text-center">
-                                    <a href="" class="btn btn-xs btn-danger"  data-toggle="modal" data-target="#modal-default">
-                                        <span class="fas fa-trash-alt"></span>
-                                    </a>
-                                </td>
-
-                                <div class="modal fade" id="modal-default">
-                                    <div class="modal-dialog">
-                                      <div class="modal-content">
-                                        <div class="modal-header">
-                                          <h4 class="modal-title">Konfirmasi</h4>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                          </button>
-                                        </div>
-                                        <div class="modal-body">
-                                          <p>Yakin Untuk Menghapus Item Ini?</p>
-                                          <p class="text-bold">PERINGATAN! Data yang Sudah Dihapus Tidak Dapat Dikembalikan</p>
-                                        </div>
-                                        <div class="modal-footer justify-content-between">
-                                          <button type="button" class="btn btn-success" data-dismiss="modal">
-                                              <span class="fas fa-times mr-1"></span>
-                                            Batalkan
-                                          </button>
-                                          <a href="{{ route('program.delete', ['id' => $program ->id]) }}" class="btn btn btn-danger">
-                                            <span class="fas fa-check mr-1"></span>
-                                            Hapus
-                                          </a>
-                                        </div>
-                                      </div>
-                                    </div>
-                                </div>
                             </tr>
                         @endforeach
                     @else
--- resources/views/programcategory/index.blade.php
+++ resources/views/programcategory/index.blade.php
@@ -19,9 +19,6 @@
                     <th class="text-center">
                         Edit
                     </th>
-                    <th class="text-center">
-                        Hapus
-                    </th>
                 </thead>
         
                 <tbody>
@@ -36,38 +33,6 @@
                                         <span class="fas fa-pencil-alt"></span>
                                     </a>
                                 </td>
-                                <td class="text-center">
-                                    <a href="" class="btn btn-xs btn-danger"  data-toggle="modal" data-target="#modal-default">
-                                        <span class="fas fa-trash-alt"></span>
-                                    </a>
-                                </td>
-
-                                <div class="modal fade" id="modal-default">
-                                    <div class="modal-dialog">
-                                      <div class="modal-content">
-                                        <div class="modal-header">
-                                          <h4 class="modal-title">Konfirmasi</h4>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                          </button>
-                                        </div>
-                                        <div class="modal-body">
-                                          <p>Yakin Untuk Menghapus Item Ini?</p>
-                                          <p class="text-bold">PERINGATAN! Data yang Sudah Dihapus Tidak Dapat Dikembalikan</p>
-                                        </div>
-                                        <div class="modal-footer justify-content-between">
-                                          <button type="button" class="btn btn-success" data-dismiss="modal">
-                                              <span class="fas fa-times mr-1"></span>
-                                            Batalkan
-                                          </button>
-                                          <a href="{{ route('programcategory.delete', ['id' => $programcategory ->id]) }}" class="btn btn btn-danger">
-                                            <span class="fas fa-check mr-1"></span>
-                                            Hapus
-                                          </a>
-                                        </div>
-                                      </div>
-                                    </div>
-                                </div>
                             </tr>
                         @endforeach
                     @else
