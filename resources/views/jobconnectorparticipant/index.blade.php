cf780bf58db78182f38ebfdacb20a828d5be947c
Michael <64026626+michaelfirdaus@users.noreply.github.com>
2021-01-21 16:45:49 +0700

    Revert "UI improvements"

--- resources/views/jobconnectorparticipant/index.blade.php
+++ resources/views/jobconnectorparticipant/index.blade.php
@@ -43,77 +43,78 @@
                 <tbody>
                     @if($jobconnectorparticipants->count() > 0)
                         @foreach ($jobconnectorparticipants as $jobconnectorparticipant)
-                            <tr>
-                                <td>
-                                    {{ $jobconnectorparticipant->participant->id }}
-                                </td>
-                                <td>
-                                    {{ $jobconnectorparticipant->participant->name }}
-                                </td>
-                                <td>
-                                    <a href="{{ $jobconnectorparticipant->participant->cv_link }}" target="_blank">{{ $jobconnectorparticipant->participant->cv_link }}</a>
-                                </td>
-                                <td>
-                                    {{ $jobconnectorparticipant->jobconnector->name }}
-                                </td>
-                                <td>
-                                    {{ $jobconnectorparticipant->date }}
-                                </td>
-                                <td>
-                                    @if( $jobconnectorparticipant->application_status == 1 )
-                                        Sedang Dalam Proses
-                                    @elseif( $jobconnectorparticipant->application_status == 2 )
-                                        Ditolak
-                                    @elseif( $jobconnectorparticipant->application_status == 3 )
-                                        Diterima
-                                    @elseif( $jobconnectorparticipant->application_status == 4 )
-                                        Dibatalkan
-                                    @else
-                                        Lainnya
-                                    @endif
-                                </td>
-                                <td class="text-center">
-                                    <a href="{{ route('jobconnectorparticipant.edit', ['id' => $jobconnectorparticipant->id]) }}" class="btn btn-xs btn-info">
-                                        <span class="fas fa-pencil-alt"></span>
-                                    </a>
-                                </td>
-                                <td class="text-center">
-                                    <a href="" class="btn btn-xs btn-danger"  data-toggle="modal" data-target="#modal-default">
-                                        <span class="fas fa-trash-alt"></span>
-                                    </a>
-                                </td>
+                                <tr>
+                                    <td>
+                                        {{ $jobconnectorparticipant->participant->id }}
+                                    </td>
+                                    <td>
+                                        {{ $jobconnectorparticipant->participant->name }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ $jobconnectorparticipant->participant->cv_link }}" target="_blank">{{ $jobconnectorparticipant->participant->cv_link }}</a>
+                                    </td>
+                                    <td>
+                                        {{ $jobconnectorparticipant->jobconnector->name }}
+                                    </td>
+                                    <td>
+                                        {{ $jobconnectorparticipant->date }}
+                                    </td>
+                                    <td>
+                                        @if( $jobconnectorparticipant->application_status == 1 )
+                                            Sedang Dalam Proses
+                                        @elseif( $jobconnectorparticipant->application_status == 2 )
+                                            Ditolak
+                                        @elseif( $jobconnectorparticipant->application_status == 3 )
+                                            Diterima
+                                        @elseif( $jobconnectorparticipant->application_status == 4 )
+                                            Dibatalkan
+                                        @else
+                                            Lainnya
+                                        @endif
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="{{ route('jobconnectorparticipant.edit', ['id' => $jobconnectorparticipant->id]) }}" class="btn btn-xs btn-info">
+                                            <span class="fas fa-pencil-alt"></span>
+                                        </a>
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="" class="btn btn-xs btn-danger"  data-toggle="modal" data-target="#modal-default">
+                                            <span class="fas fa-trash-alt"></span>
+                                        </a>
+                                    </td>
+    
+                                    <div class="modal fade" id="modal-default">
+                                        <div class="modal-dialog">
+                                          <div class="modal-content">
+                                            <div class="modal-header">
+                                              <h4 class="modal-title">Konfirmasi</h4>
+                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                              </button>
+                                            </div>
+                                            <div class="modal-body">
+                                              <p>Yakin Untuk Menghapus Item Ini?</p>
+                                              <p class="text-bold">PERINGATAN! Data yang Sudah Dihapus Tidak Dapat Dikembalikan</p>
+                                            </div>
+                                            <div class="modal-footer justify-content-between">
+                                              <button type="button" class="btn btn-success" data-dismiss="modal">
+                                                  <span class="fas fa-times mr-1"></span>
+                                                Batalkan
+                                              </button>
+                                              <a href="{{ route('jobconnectorparticipant.delete', ['id' => $jobconnectorparticipant->id]) }}" class="btn btn btn-danger">
+                                                <span class="fas fa-check mr-1"></span>
+                                                Hapus
+                                              </a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
 
-                                <div class="modal fade" id="modal-default">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title">Konfirmasi</h4>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>Yakin Untuk Menghapus Item Ini?</p>
-                                            <p class="text-bold">PERINGATAN! Data yang Sudah Dihapus Tidak Dapat Dikembalikan</p>
-                                        </div>
-                                        <div class="modal-footer justify-content-between">
-                                            <button type="button" class="btn btn-success" data-dismiss="modal">
-                                                <span class="fas fa-times mr-1"></span>
-                                            Batalkan
-                                            </button>
-                                            <a href="{{ route('jobconnectorparticipant.delete', ['id' => $jobconnectorparticipant->id]) }}" class="btn btn btn-danger">
-                                            <span class="fas fa-check mr-1"></span>
-                                            Hapus
-                                            </a>
-                                        </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </tr>
+                                </tr>
                         @endforeach
                     @else
                         <tr>
-                            <th colspan="8" class="text-center">Tidak ada job connector yang terdaftar.</th>
+                            <th colspan="6" class="text-center">Tidak ada job connector yang terdaftar.</th>
                         </tr>
                     @endif
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Course-Net CRM</title>
+    <title>Admin Course-Net</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -44,11 +44,11 @@
   @if(Auth::check())
     <div class="wrapper">
       <!-- Navbar -->
-      <nav class="main-header navbar navbar-expand navbar-black navbar-dark">
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i><span>  Menu</span></a>
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
           </li>
         </ul>
 
@@ -84,7 +84,7 @@
         <a href="index3.html" class="brand-link">
           <img src={{ asset('assets/logo-cn.png') }} alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
               style="opacity: .8">
-          <span class="brand-text font-weight-light">Course-Net CRM</span>
+          <span class="brand-text font-weight-light">Course-Net Indonesia</span>
         </a>
 
         <!-- Sidebar -->
@@ -290,7 +290,7 @@
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
-                <h1 class="m-0 text-black text-bold">@yield('header')</h1>
+                <h1 class="m-0 text-dark">@yield('header')</h1>
               </div><!-- /.col -->
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
@@ -313,7 +313,7 @@
         <!-- /.content -->
       </div>
       <!-- /.content-wrapper -->
-      <footer class="main-footer bg-dark">
+      <footer class="main-footer">
         <strong>Copyright &copy; 2020-2021. Created By Michael.</strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
@@ -332,10 +332,10 @@
 
   @if(!Auth::check())
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-black shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('assets/logo-cn-panjang.png') }}" height=30px width=120px>
+                Admin Internal Course-Net
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
@@ -351,11 +351,8 @@
                 <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
                     @guest
-                        {{-- <li class="nav-item">
-                            <a class="nav-link text-bold" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li> --}}
                         <li class="nav-item">
-                          <div class="text-bold">Course-Net CRM</div>
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                             <li class="nav-item">
@@ -364,7 +361,7 @@
                         @endif
                     @else
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-bold" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
 
